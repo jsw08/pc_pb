bfe5e846f54f600db669572fa41eefb58c1cf442
jsw08 <jurnwubben@gmail.com>
2024-02-01 11:38:08 +0100

    fixy fixy

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
     import PowerMangager from "./components/power_mangager.svelte";
-
+    import { pcs } from "./store";
 </script>
 
 <PowerMangager/>
+
+{#each $pcs as pc}
+    <PowerMangager title={pc.name} ntfyUrl={pc.url}/>
+{/each}
 
 <!-- <main class="w-full h-full p-4">
 
